3581b4a8492a162eeba55555eeceab1da4349fc1
1U23CA040GUNASEKAR <139735207+1U23CA040GUNASEKAR@users.noreply.github.com>
2024-01-18 09:45:20 +0000

    35

--- Person.java
+++ Person.java
@@ -20,8 +20,8 @@ public class Person
     {
         person1 p1=new person1( "John","Doe",30);
         person1 p2=new person1( "Alice","Smith",25);
-        System.out.println("person 1:"+p1.getfullname());
-        System.out.println("person 2:"+p2.getfullname());
+        System.out.println("Person 1:"+p1.getfullname());
+        System.out.println("Person 2:"+p2.getfullname());
         double a1 =((p1.age+p2.age)/2);
         System.out.println("Average Age:"+a1);
     }
